a515a0f12e08c59c8f129672e81b90f23b1f404a
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-01-13 15:47:48 +0000

    add vercel.json

---
+++ www/browser/chunk-AGM5QK4D.js
@@ -0,0 +1,1 @@
+import{a,b,c,d,e}from"./chunk-5YEN3EPI.js";import"./chunk-MCRJI3T3.js";import"./chunk-PVZQOOQ7.js";import"./chunk-U2CNLB2V.js";import"./chunk-RW4GY4BD.js";export{e as MENU_BACK_BUTTON_PRIORITY,d as OVERLAY_BACK_BUTTON_PRIORITY,b as blockHardwareBackButton,a as shouldUseCloseWatcher,c as startHardwareBackButton};
---
+++ www/browser/chunk-L7DNWWVU.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-7QLMNM35.js";import"./chunk-FF3P7ZBM.js";import"./chunk-G3CV3VGG.js";import"./chunk-FANUMIDU.js";import"./chunk-SB67TZC5.js";import"./chunk-QXT6YJJ4.js";import"./chunk-LF5XB4YN.js";import"./chunk-RW4GY4BD.js";export{a as mdTransitionAnimation};
